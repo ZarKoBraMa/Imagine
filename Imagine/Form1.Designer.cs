9d202b6209a0dcf9c26df69f4c49332a61a3f2d8
Bakurski Žarko <zarkoba@gmail.com>
2024-08-18 15:50:37 +0200

    labels

--- Imagine/Form1.Designer.cs
+++ Imagine/Form1.Designer.cs
@@ -28,11 +28,165 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        pictureBox1 = new PictureBox();
+        label1 = new Label();
+        label2 = new Label();
+        label3 = new Label();
+        lblDPI = new Label();
+        lblHeight = new Label();
+        lblWidth = new Label();
+        btnUpload = new Button();
+        lblWidthMM = new Label();
+        lblHeightMM = new Label();
+        label6 = new Label();
+        label7 = new Label();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        SuspendLayout();
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Location = new Point(12, 12);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(651, 810);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(669, 12);
+        label1.Name = "label1";
+        label1.Size = new Size(64, 15);
+        label1.TabIndex = 1;
+        label1.Text = "Image DPI:";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(669, 48);
+        label2.Name = "label2";
+        label2.Size = new Size(104, 15);
+        label2.TabIndex = 2;
+        label2.Text = "Image height (px):";
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(669, 75);
+        label3.Name = "label3";
+        label3.Size = new Size(100, 15);
+        label3.TabIndex = 3;
+        label3.Text = "Image width (px):";
+        // 
+        // lblDPI
+        // 
+        lblDPI.AutoSize = true;
+        lblDPI.Location = new Point(779, 12);
+        lblDPI.Name = "lblDPI";
+        lblDPI.Size = new Size(13, 15);
+        lblDPI.TabIndex = 4;
+        lblDPI.Text = "0";
+        // 
+        // lblHeight
+        // 
+        lblHeight.AutoSize = true;
+        lblHeight.Location = new Point(779, 48);
+        lblHeight.Name = "lblHeight";
+        lblHeight.Size = new Size(13, 15);
+        lblHeight.TabIndex = 5;
+        lblHeight.Text = "0";
+        // 
+        // lblWidth
+        // 
+        lblWidth.AutoSize = true;
+        lblWidth.Location = new Point(779, 75);
+        lblWidth.Name = "lblWidth";
+        lblWidth.Size = new Size(13, 15);
+        lblWidth.TabIndex = 6;
+        lblWidth.Text = "0";
+        // 
+        // btnUpload
+        // 
+        btnUpload.Location = new Point(669, 177);
+        btnUpload.Name = "btnUpload";
+        btnUpload.Size = new Size(139, 23);
+        btnUpload.TabIndex = 7;
+        btnUpload.Text = "Upload Image";
+        btnUpload.UseVisualStyleBackColor = true;
+        btnUpload.Click += btnUpload_Click;
+        // 
+        // lblWidthMM
+        // 
+        lblWidthMM.AutoSize = true;
+        lblWidthMM.Location = new Point(779, 141);
+        lblWidthMM.Name = "lblWidthMM";
+        lblWidthMM.Size = new Size(13, 15);
+        lblWidthMM.TabIndex = 11;
+        lblWidthMM.Text = "0";
+        // 
+        // lblHeightMM
+        // 
+        lblHeightMM.AutoSize = true;
+        lblHeightMM.Location = new Point(779, 114);
+        lblHeightMM.Name = "lblHeightMM";
+        lblHeightMM.Size = new Size(13, 15);
+        lblHeightMM.TabIndex = 10;
+        lblHeightMM.Text = "0";
+        // 
+        // label6
+        // 
+        label6.AutoSize = true;
+        label6.Location = new Point(669, 141);
+        label6.Name = "label6";
+        label6.Size = new Size(109, 15);
+        label6.TabIndex = 9;
+        label6.Text = "Image width (mm):";
+        // 
+        // label7
+        // 
+        label7.AutoSize = true;
+        label7.Location = new Point(669, 114);
+        label7.Name = "label7";
+        label7.Size = new Size(113, 15);
+        label7.TabIndex = 8;
+        label7.Text = "Image height (mm):";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(877, 837);
+        Controls.Add(lblWidthMM);
+        Controls.Add(lblHeightMM);
+        Controls.Add(label6);
+        Controls.Add(label7);
+        Controls.Add(btnUpload);
+        Controls.Add(lblWidth);
+        Controls.Add(lblHeight);
+        Controls.Add(lblDPI);
+        Controls.Add(label3);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Controls.Add(pictureBox1);
+        Name = "Form1";
+        Text = "Form1";
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private PictureBox pictureBox1;
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Label lblDPI;
+    private Label lblHeight;
+    private Label lblWidth;
+    private Button btnUpload;
+    private Label lblWidthMM;
+    private Label lblHeightMM;
+    private Label label6;
+    private Label label7;
 }
--- Imagine/Form1.cs
+++ Imagine/Form1.cs
@@ -6,4 +6,40 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private void btnUpload_Click(object sender, EventArgs e)
+    {
+        // Open a file dialog to select an image
+        using (OpenFileDialog ofd = new OpenFileDialog())
+        {
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Load the selected image
+                Image image = Image.FromFile(ofd.FileName);
+                pictureBox1.Image = image;
+
+                // Display the image width, height, and DPI
+                lblWidth.Text = image.Width.ToString();
+                lblHeight.Text = image.Height.ToString();
+                lblDPI.Text = image.HorizontalResolution.ToString();
+
+                // Calculate dimensions in millimeters
+                float widthInches = image.Width / image.HorizontalResolution;
+                float heightInches = image.Height / image.VerticalResolution;
+
+                float widthMM = widthInches * 25.4f; // 1 inch = 25.4 mm
+                float heightMM = heightInches * 25.4f;
+
+                // Display dimensions in millimeters
+                lblWidthMM.Text = $"{widthMM:F2}";
+                lblHeightMM.Text = $"{heightMM:F2}";
+
+                // Optionally, resize the PictureBox to fit the image
+                pictureBox1.Width = image.Width;
+                pictureBox1.Height = image.Height;
+            }
+        }
+    }
 }
